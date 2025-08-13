025895d1cef4735e3278870e2c30d95faa0d1314
pranav-archi <158431652+pranav-archi@users.noreply.github.com>
2025-08-13 15:45:45 +0530

    Add files via upload

---
+++ lab1d.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(2,OUTPUT);
+  pinMode(9,INPUT);
+}
+void loop()
+{
+ int status=0;
+  status=digitalRead(9);
+  if(status==1){
+  	digitalWrite(2,HIGH);
+  }
+  else{
+    digitalWrite(2,LOW);
+  }
+}
